a24065ae8a50e3188525edf1365027bc3300ca90
Sébastien David <sebastien-david@outlook.com>
2016-03-27 13:14:25 +0200

    add unit test

---
+++ test/unit/index.spec.js
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
+
+describe(__filename, () => {
+  let ReactDOM;
+
+  before(() => {
+    global.document = {
+      getElementById: sinon.spy()
+    };
+    ReactDOM = {
+      '@noCallThru': true,
+      render: sinon.spy()
+    };
+    proxyquire('src/index', {
+      'src/app': {
+        '@noCallThru': true
+      },
+      'src/store/configureStore': Object.assign(sinon.spy(), {
+        '@noCallThru': true
+      }),
+      react: {
+        '@noCallThru': true,
+        createElement: sinon.spy()
+      },
+      'react-redux': {
+        '@noCallThru': true
+      },
+      'react-dom': ReactDOM
+    });
+  });
+
+  after(() => {
+    global.document = null;
+  });
+
+  it('should render app', () => {
+    expect(ReactDOM.render.callCount).to.equal(1);
+  });
+});
